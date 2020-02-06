b556f07a26075814a8e4f0cb7cdad2b7b819b8b1
Mayank Tiwari <MayankA.Tiwari@lntinfotech.com>
2020-02-06 12:30:50 +0530

    'found'

--- uploaddata.js
+++ uploaddata.js
@@ -1,6 +1,10 @@
 const CognitiveServicesCredentials = require("@azure/ms-rest-js");
 const TextAnalyticsAPIClient = require("@azure/cognitiveservices-textanalytics");
-
+var docx = require('./docx.js');
+const path = require("path");
+var request = require('request');
+const mongodb = require('mongodb');
+const assert = require('assert');
 
 //const fileNameArray = ["test.pptx"]
 exports.getData = function(answer, obj,cb) {
